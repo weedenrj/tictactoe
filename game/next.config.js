1e377d72c953e20e661c7432dbe0538d7746710a
weedenrj <robertweeden1997@gmail.com>
2022-07-13 17:39:59 -0500

    init

---
+++ game/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+    return config
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    formats: ['image/webp']
+  }
+}
